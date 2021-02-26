e82a905bfd27722387067174dc146c22cf8c40a5
oleg <litwinenkooleg371@gmail.com>
2021-02-26 21:56:06 +0200

    +++

--- src/App.js
+++ src/App.js
@@ -1,7 +1,54 @@
-import PropTypes from 'prop-types';
+import React from 'react';
 
-function App() {
-  return <div className="App"></div>;
+import Section from './Components/Section';
+import FeedbackOptions from './Components/FeedbackOptions';
+import Titel from './Components/Titel';
+import Statistics from './Components/Statistics';
+
+class App extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  incrementFeedback = option => {
+    this.setState(prevState => {
+      return { [option]: prevState[option] + 1 };
+    });
+  };
+
+  render() {
+    const { bad, good, neutral } = this.state;
+    const options = Object.keys(this.state);
+
+    const total = bad + good + neutral;
+
+    const countPositiveFeedbackPercentage = () => {
+      return Math.round((good * 100) / total);
+    };
+
+    return (
+      <Section>
+        <Titel titel="Please leave feedback" />
+
+        {options.map(item => (
+          <FeedbackOptions
+            key={item}
+            option={item}
+            onLeaveFeedback={this.incrementFeedback}
+          />
+        ))}
+        <Statistics
+          good={good}
+          bad={bad}
+          neutral={neutral}
+          total={total}
+          positivePercentage={countPositiveFeedbackPercentage()}
+        />
+      </Section>
+    );
+  }
 }
 
 export default App;
